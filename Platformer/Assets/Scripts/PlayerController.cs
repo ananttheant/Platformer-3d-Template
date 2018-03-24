5da83d423d1fe407c934677bf7cee8719dda3bd3
Anant Sharma <ananttheant4444@gmail.com>
2018-03-24 20:30:25 +0530

    Camera Follow the player function added and bottom ground is added.

--- Platformer/Assets/Scripts/PlayerController.cs
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -16,6 +16,12 @@ public class PlayerController : MonoBehaviour
 
     public AudioSource CoinSound;
 
+    //camera distance on z
+    public float CameraDistZ = 6;
+
+    //value that you fell on bottom
+    private float minY = -1.6f;
+
 	// Use this for initialization
 	void Start ()
 	{
@@ -24,14 +30,20 @@ public class PlayerController : MonoBehaviour
 
         //get size of the play
 	    size = col.bounds.size;
+
+        //set the camera position initially
+        CameraFollowPlayer();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		WalkHandler();
         JumpHandler();
-
+	    CameraFollowPlayer();
+	    FallHandler();
 	}
+
+    
 
     void WalkHandler()
     {
@@ -105,17 +117,17 @@ public class PlayerController : MonoBehaviour
         Vector3 midFace = transform.position + new Vector3(0, -size.y / 2 + 0.01f, 0);
 
         //raycast (check if grounded)          
-        //TODO fix this thing or change to feet
+        
         bool grounded1 = Physics.Raycast(corner1, -Vector3.up, 0.01f);
-        Debug.DrawRay(corner1, -Vector3.up * 0.1f);
+        //Debug.DrawRay(corner1, -Vector3.up * 0.1f);
         bool grounded2 = Physics.Raycast(corner2, -Vector3.up, 0.01f);
-        Debug.DrawRay(corner2, -Vector3.up * 0.1f);
+        //Debug.DrawRay(corner2, -Vector3.up * 0.1f);
         bool grounded3 = Physics.Raycast(corner3, -Vector3.up, 0.01f);
-        Debug.DrawRay(corner3, -Vector3.up * 0.1f);
+        //Debug.DrawRay(corner3, -Vector3.up * 0.1f);
         bool grounded4 = Physics.Raycast(corner4, -Vector3.up, 0.01f);
-        Debug.DrawRay(corner4, -Vector3.up * 0.1f);
+        //Debug.DrawRay(corner4, -Vector3.up * 0.1f);
         bool midFaceCheck = Physics.Raycast(midFace, -Vector3.up, 0.01f);
-        Debug.DrawRay(midFace, -Vector3.up * 0.1f);
+        //Debug.DrawRay(midFace, -Vector3.up * 0.1f);
 
         return (grounded1 || grounded2 || grounded3 || grounded4 || midFaceCheck);
     }
@@ -143,6 +155,30 @@ public class PlayerController : MonoBehaviour
             //print("you made it");
             //Increse level
             GameManager.Instance.IncreseLevel();
+        }
+
+    }
+
+    void CameraFollowPlayer()
+    {
+        //grab the camera
+        Vector3 cameraPosition = Camera.main.transform.position;
+
+        //modify it's position according to Camera Distace on Z
+        cameraPosition.z = transform.position.z - CameraDistZ;
+
+        //set camera position
+        Camera.main.transform.position = cameraPosition;
+
+
+    }
+
+    //check if the player fell
+    private void FallHandler()
+    {
+        if (transform.position.y <= minY)
+        {
+            GameManager.Instance.GameOver();
         }
     }
 }
