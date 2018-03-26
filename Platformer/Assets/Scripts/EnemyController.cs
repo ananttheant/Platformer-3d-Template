3154555d34a2a9865db5f9b4f549266b1df36fa6
Anant Sharma <ananttheant4444@gmail.com>
2018-03-26 18:23:14 +0530

    update

---
+++ Platformer/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float Speed = 3;
+    private int _direction = 1;
+    public float RangeY = 2; //y
+
+    private Vector3 _initialLocation;
+	// Use this for initialization
+	void Start ()
+	{
+	    _initialLocation = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        //factor by which it should move
+	    float factor = _direction == -1 ? 1.2f : 1;
+
+        //speed + direction
+	    float movementY = factor * Speed * Time.deltaTime * _direction;
+
+        //new position in that frame after adding movement(i.e speed and direction covered in that frame)
+	    float newY = transform.position.y + movementY;
+
+        //abs = mod 
+	    if (Mathf.Abs(newY - _initialLocation.y) > RangeY)
+	    {
+	        _direction *= -1;
+	    }
+        //if we can move further , move further
+	    else
+	    {
+            transform.position += new Vector3(0,movementY,0);
+	    }
+
+	}
+}
